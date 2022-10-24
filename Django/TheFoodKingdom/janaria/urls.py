ba05116923bf41a33488655375e746daecfffdca
Maitane <gallastegui.maitane@uni.eus>
2022-10-24 13:50:02 +0200

    Karta orria egin

--- Django/TheFoodKingdom/janaria/urls.py
+++ Django/TheFoodKingdom/janaria/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index,name='index')]
+    path('',views.index,name='index'),
+    path('karta/',views.karta,name='karta'),
+    ]
--- Django/TheFoodKingdom/janaria/views.py
+++ Django/TheFoodKingdom/janaria/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def karta(request):
+    return render(request, 'karta.html')
