03f46775712e451b4ab621d940faaee15f43c4a2
Maitane <gallastegui.maitane@uni.eus>
2022-10-25 08:06:51 +0200

    Navbar-aren linkak konpondu

--- Django/TheFoodKingdom/janaria/urls.py
+++ Django/TheFoodKingdom/janaria/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns=[
     path('',views.index,name='index'),
     path('karta/',views.karta,name='karta'),
-    path('karta/index/',views.index_bueltatu,name='index_bueltatu'),
     path('kokapena/',views.kokapena,name='kokapena'),
     ]
