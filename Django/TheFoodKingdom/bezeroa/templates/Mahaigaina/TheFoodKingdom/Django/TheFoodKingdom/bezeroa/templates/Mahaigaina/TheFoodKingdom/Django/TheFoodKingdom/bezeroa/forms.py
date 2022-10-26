17b614134a2ad0b0d41ac5c7c9f196d3286dc630
garateHegoi <garate.hegoi@uni.eus>
2022-10-26 09:35:15 +0200

    konponduta

--- Django/TheFoodKingdom/bezeroa/templates/Mahaigaina/TheFoodKingdom/Django/TheFoodKingdom/bezeroa/templates/Mahaigaina/TheFoodKingdom/Django/TheFoodKingdom/bezeroa/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Erabiltzaileak
-
-class ErregistroaForm(forms.ModelForm):
-
-    class Meta:
-        model = Erabiltzaileak
-        fields = ('username', 'email','first_name','last_name','password')
