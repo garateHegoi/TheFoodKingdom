eda4ac9c0c83e272ff17fca89f47ab3c460a4a7d
MaitaneG <Maitane>
2022-10-31 11:26:11 +0100

    Karta paginan depende zer kategoria den produktu batzuk edo beste batzuk agertzen dira

--- Django/TheFoodKingdom/janaria/urls.py
+++ Django/TheFoodKingdom/janaria/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns=[
     path('',views.index,name='index'),
     path('karta/',views.karta,name='karta'),
+    path('karta_sailkatua/<str:submota>',views.karta_sailkatua,name='karta_sailkatua'),
     path('kokapena/',views.kokapena,name='kokapena'),
     path('erregistroa/',views.erregistroa,name='erregistroa'),
     path('profila/',views.profila,name='profila'),
--- Django/TheFoodKingdom/janaria/views.py
+++ Django/TheFoodKingdom/janaria/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import Janariak
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def karta(request):
-    return render(request, 'karta.html')
+    janariak = Janariak.objects.all()[:9]
+    return render(request, 'karta.html',{'janariak':janariak})
+
+def karta_sailkatua(request, submota):
+    janariak = Janariak.objects.filter(submota=submota)
+    return render(request, 'karta.html',{'janariak':janariak})
 
 def kokapena(request):
     return render(request, 'kokapena.html')
