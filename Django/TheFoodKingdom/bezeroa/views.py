6a7d98aedd78059cae1a25b7183e488cfe2e2afd
Maitane <gallastegui.maitane@uni.eus>
2022-11-03 09:30:36 +0100

    Profila ondo eguneratu

--- Django/TheFoodKingdom/bezeroa/views.py
+++ Django/TheFoodKingdom/bezeroa/views.py
@@ -13,6 +13,25 @@ def profilaaldatu(request, id_erabiltzaile):
     bezeroa= Bezeroak.objects.get(id_erabiltzaile=id_erabiltzaile)
     return render(request, 'profilaaldatu.html',{'bezeroa':bezeroa})
 
+def profilaaldatudb(request, id):
+    erabiltzailea = request.POST['username']
+    izena=request.POST['first_name']
+    abizena=request.POST['last_name']
+    telefonoa=request.POST['telefono_zenbakia']
+
+    user= User.objects.get(id=id)
+
+    user.username=erabiltzailea
+    user.first_name=izena
+    user.last_name=abizena
+
+    bezero= Bezeroak.objects.get(id_erabiltzaile=id)
+    bezero.telefono_zenbakia=telefonoa
+
+    user.save()
+    bezero.save()
+
+    return redirect('profila')
 
 def erregistratu(request):
     erabiltzailea = request.POST['username']
