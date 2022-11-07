fbb4b1f2f67f77acf99de2488e179ac9342c1512
garateHegoi <71440711+garateHegoi@users.noreply.github.com>
2022-11-07 10:13:03 +0100

    erosketapruebaViews

--- Django/TheFoodKingdom/erosketa/urls.py
+++ Django/TheFoodKingdom/erosketa/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('saskia/',views.saskia,name='saskia'),
-    path('gehitu_saskira/',views.gehitu_saskira,name='gehitu_saskira'),
-    path('ezabatu_saskia/',views.ezabatu_saskia,name='ezabatu_saskia'),
-    path('saskia_info/<int:id>',views.saskia_info,name='saskia_info'),
+urlpatterns = [
+    path('saskia/', views.saskia, name='saskia'),
+    path('erosketa/', views.erosketa, name='erosketa'),
+    path('erosketaOrd/', views.erosketaOrd, name='erosketaOrd'),
 ]
--- Django/TheFoodKingdom/erosketa/views.py
+++ Django/TheFoodKingdom/erosketa/views.py
@@ -1,36 +1,15 @@
-from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from janaria.models import Janariak
-
-from .models import Saskiak
+from django.shortcuts import render
 
 # Create your views here.
+
+
 def saskia(request):
-    saskia = Saskiak.objects.all
-    return render(request, 'saskia.html', {'saskia':saskia})
-
-def gehitu_saskira(request):
-    id = request.POST['id']
-    kop = request.POST.get('kop')
-    guzt = request.POST.get('guztira')
-    # Gehitu erosketa
-    saskia = Saskiak(janari_id_id=id, kantitate_kopurua=kop, guztira=guzt)
-    saskia.save()
-    return redirect('saskia')
+    return render(request, 'saskia.html')
 
 
-def ezabatu_saskia(request):
-    saskia_id = int(request.POST.get('id'))
-    print(saskia_id)
-    saskia = Saskiak.objects.get(id=saskia_id)
+def erosketa(request):
+    return render(request, 'erosketa.html')
 
-    saskia_bid = list(Saskiak.objects.filter(id=saskia_id).values())
 
-    saskia.delete()
-
-    return JsonResponse(saskia_bid, safe=False)
-
-def saskia_info(request, id):
-    saskia = Saskiak.objects.get(id=id)
-    produktua = Janariak.objects.get(id=saskia.janari_id.id)
-    return render(request, 'produktua.html', {'saskia':saskia, 'produktua':produktua})
+def erosketaOrd(request):
+    return render(request, 'erosketaOrd.html')
